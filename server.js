59090a2685c69ffbb45e194f779d83fa53c1956f
cmsniperline@gmail.com <cmsniperline@gmail.com>
2019-08-27 01:37:14 +0530

    POST

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/',(req,res) => {
 	res.send("Hello to Web 2.0");
 });
 
+app.post('/',(req,res) => {
+	let email = req.body.email;
+	let amount = req.body.amount;
+
+	res.send({"amount" : amount, "email" : email});
+});
+
 app.listen(4000,() => {
-	console.log("App is running on Port 2000")
+	console.log("App is running on Port 4000")
 });
