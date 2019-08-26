5a270cc6e0e42e4b7ff79fcd252084a213fd6c3f
cmsniperline@gmail.com <cmsniperline@gmail.com>
2019-08-27 01:58:27 +0530

    adding db.js

---
+++ db.js
@@ -0,0 +1,30 @@
+var mysql = require("mysql");
+var db_config = {
+	host : '127.0.0.1',
+	user : 'root',
+	password : 'Sniper*047',
+	database : 'webapp'
+}
+
+var connection;
+
+function handleConnection() {
+	connection = mysql.createConnection(db_config);
+	connection.connect(function(err) {
+		if(err) {
+			console.log('Error when connection to DB :',err);
+			setTimeout(handleConnection,2000);
+		}
+	});
+	connection.on('error',function(err) {
+		if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+			handleConnection();
+		}else {
+			throw err;
+		}
+	});
+}
+
+handleConnection();
+
+module.exports = connection;
